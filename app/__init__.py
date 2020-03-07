7b3fe1a0fe63fcedfa16eae1a167a1b242ecac8f
0903lokchan <0903lokchan@gmail.com>
2020-03-07 22:41:52 +1100

    Interface upgrade; adopt Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
 def create_app():
     app = Flask(__name__)
+
+    bootstrap.init_app(app)
 
     from app.dice_cal import bp as dice_cal_bp
     app.register_blueprint(dice_cal_bp)
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
 
     return app
 

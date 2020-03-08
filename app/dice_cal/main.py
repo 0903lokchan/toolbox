450f553ddf3f30e7ebf16a20d1db20bb04e47398
0903lokchan <0903lokchan@gmail.com>
2020-03-08 11:50:11 +1100

    add rounding to dice_cal

--- app/dice_cal/main.py
+++ app/dice_cal/main.py
@@ -1,8 +1,11 @@
 from decimal import Decimal
 
-def simulate(dice, side):
+def simulate(dice: int, side: int):
 
     case_count = side**dice
+    if case_count > 10000:
+        return "Sorry, the server-side function only support simulation with case amount less than 10000. Please use other values."
+
     case_probability = Decimal('1')/case_count
     list_case = []
     target_case = []
@@ -27,10 +30,10 @@ def simulate(dice, side):
         result = []
 
         for i in list(range(min_sum, max_sum+1)):
-            p = Decimal(list_case.count(i) * case_probability)
+            p = round(list_case.count(i) * case_probability * 100, 6)
             result.append({'result': i, 'possibility': p})
 
     output = ""
     for item in result:
-        output += f"Result: {item['result']}    Possibility: {item['possibility']*100}%<br>"
+        output += f"Result: {item['result']}    Possibility: {item['possibility']}%<br>"
     return output
